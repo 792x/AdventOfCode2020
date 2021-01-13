2d4eb2ed48705fa6054b094cf205982794d09041
Casper Siksma <casper@actinius.com>
2021-01-13 18:23:37 +0100

    Day 9 part 2

--- src/aoc9/main.rs
+++ src/aoc9/main.rs
@@ -7,22 +7,48 @@ use std::path::Path;
 pub fn run() {
     let lines = read_puzzle_input();
     let numbers = parse_numbers(lines);
-    find_bad_numbers(numbers, 25);
+    let bad_number = find_bad_number(&numbers, 25);
+    println!("Bad number: {}", &bad_number);
+    let set = find_sum_set(&numbers, bad_number);
+    println!("Contiguous set min and max sum: {}", set);
+
 
 }
 
-fn find_bad_numbers(numbers: Vec<i64>, preamble_size: i64) {
+fn find_sum_set(numbers: &Vec<i64>, bad_number: i64) -> i64 {
+    let mut first = 0;
+    let mut last = 0;
+    let mut sum = 0;
+
+    while sum != bad_number {
+        while sum < bad_number {
+            sum += numbers[last];
+            last += 1;
+        }
+
+        while sum > bad_number {
+            sum -= numbers[first];
+            first += 1;
+        }
+    }
+
+    let slice = &numbers[first..last];
+    println!("Contiguous set found: {:?}", slice);
+    slice.iter().min().unwrap() + slice.iter().max().unwrap()
+}
+
+fn find_bad_number(numbers: &Vec<i64>, preamble_size: i64) -> i64{
     let mut window: Vec<i64> = Vec::new();
     for number in numbers {
         if window.len() as i64 > preamble_size {
             window.drain(..1);
         }
-        if window.len() as i64 >= preamble_size && !find_two_sum(&window, number) {
-            println!("Bad number: {}", number);
-            break;
+        if window.len() as i64 >= preamble_size && !find_two_sum(&window, *number) {
+            return *number;
         }
-        window.push(number);
+        window.push(*number);
     }
+    return -1;
 }
 
 fn find_two_sum(window: &Vec<i64>, target: i64) -> bool {
